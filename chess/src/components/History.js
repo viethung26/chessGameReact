1d85debd6f2172756d460cdb4108d6d69e546676
hideki <viethungpro97@gmail.com>
2018-10-17 06:59:33 +0700

    redesign

--- chess/src/components/History.js
+++ chess/src/components/History.js
@@ -18,9 +18,10 @@ class History extends Component {
 				<hr/>
 				<p>Turn: {turn+1} - Team: {turn%2?1:2}</p>	
 				<ul>
-					<li><button className="btn2" onClick={()=>this.props.jumpTo(turn-1)}>Previous</button> 
-					<button className="btn2" onClick={()=>this.props.jumpTo(turn+1)}>Next</button></li>	
-					<li key={0}><button className="button" onClick={()=>this.props.jumpTo(0)}>New Game</button></li>
+					<li><button className="btn2" onClick={()=>this.props.jumpTo(turn-1)}>
+					<i className="fa fa-backward"></i> Previous</button> 
+					<button className="btn2" onClick={()=>this.props.jumpTo(turn+1)}>Next <i className="fa fa-forward"></i></button></li>	
+					<li key={0}><button className="button" onClick={()=>this.props.jumpTo(0)}><i className="fa fa-refresh"></i> New Game</button></li>
 					{this.setList()}
 				</ul>	
 			</div>
